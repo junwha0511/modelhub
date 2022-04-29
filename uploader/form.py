37c60384a4893e33b9d24d3eb404d058d2b5de5b
준화 <junwha0511@naver.com>
2022-04-29 22:44:35 +0900

    Add form for deployer

---
+++ uploader/form.py
@@ -0,0 +1,32 @@
+### Implement train logic with modifiable parameter
+def train_model(input, p1, p2, p3, ...):
+    ### preprocess
+
+    model = Sequential([
+        '''
+        Example:
+        keras.layers.Conv2D(256, 3, activation="relu", padding=p3) <- here, user can modify p3
+        '''
+        ### Implement model here
+    ])
+
+    ### train 
+    
+
+### Implement test logic with trained model
+def test_model(input):
+    ### 
+    pass 
+
+### Implement running logic 
+def run_train():
+    input = open("./dataset.pickle") # Here you must use pickle
+    p1 = ...
+    p2 = ...
+    ...
+    
+    ### we will intercept here
+    train_model(input, p1, p2, p3, ...)
+
+def run_test(img):
+    return test_model(img)
